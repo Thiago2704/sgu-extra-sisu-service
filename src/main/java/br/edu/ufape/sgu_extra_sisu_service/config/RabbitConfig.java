e2a7a102296a0fedaebb18d2acd7ec3d47a3261f
Thiago2704 <thiagoaraujo909090@gmail.com>
2025-12-19 14:36:11 -0300

    feat: configura Feign Client, RabbitMQ e RestTemplate para comunicação entre serviços

---
+++ src/main/java/br/edu/ufape/sgu_extra_sisu_service/config/RabbitConfig.java
@@ -0,0 +1,31 @@
+package br.edu.ufape.sgu_extra_sisu_service.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String EXCHANGE = "auth-role-exchange";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
---
+++ src/main/java/br/edu/ufape/sgu_extra_sisu_service/config/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.sgu_extra_sisu_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
